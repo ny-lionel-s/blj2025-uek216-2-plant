4608e34a6a321ad897681707977f080ffc05f473
ny-julian-g <julian.gasser@noseryoung.ch>
2026-01-21 11:12:28 +0100

    basic code for states and sensor

--- Src/uek216_plant_code/uek216_plant_code.ino
+++ Src/uek216_plant_code/uek216_plant_code.ino
@@ -1,1 +1,44 @@
-test
+#define GREEN 23
+#define YELLOW 22
+#define RED 21
+#define SENSOR 26
+
+int max_humid = 10000;   // to wet
+int min_humid = 50;   // to dry
+
+void setup() {
+  pinMode(GREEN, OUTPUT);
+  pinMode(YELLOW, OUTPUT);
+  pinMode(RED, OUTPUT);
+
+  pinMode(SENSOR, INPUT);
+
+  Serial.begin(115200);
+}
+
+void loop() {
+  int moisture = analogRead(SENSOR);
+
+  Serial.print("Feuchtigkeit: ");
+  Serial.println(moisture);
+
+  // LOW all LED
+  digitalWrite(GREEN, LOW);
+  digitalWrite(YELLOW, LOW);
+  digitalWrite(RED, LOW);
+
+  if (moisture < min_humid) {
+    // to dry
+    digitalWrite(YELLOW, HIGH);
+  }
+  else if (moisture > max_humid) {
+    // to wet
+    digitalWrite(RED, HIGH);
+  }
+  else {
+    // good
+    digitalWrite(GREEN, HIGH);
+  }
+
+  delay(500);
+}
