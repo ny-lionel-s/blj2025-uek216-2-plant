ddb0ee40b417b000e941f958a498faceb6c2904b
ny-julian-g <julian.gasser@noseryoung.ch>
2026-01-21 13:56:00 +0100

    constantely publish values

--- Src/uek216_plant_code/uek216_plant_code.ino
+++ Src/uek216_plant_code/uek216_plant_code.ino
@@ -3,25 +3,24 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
-// Pins 
+// Pins
 #define GREEN 23
-#define YELLOW 19
-#define RED 18
-#define SENSOR 26
+#define YELLOW 25
+#define RED 26
+#define SENSOR 32
 
 // Schwellen
-int max_humid = 10000;   // zu nass
-int min_humid = 50;      // zu trocken
+int max_humid = 2800;  // zu nass
+int min_humid = 1200;  // zu trocken
 
-// WLAN 
+// WLAN
 const char* ssid = "GuestWLANPortal";
-
 
 // MQTT
 const char* mqtt_server = "10.10.2.127";
 const char* mqtt_topic = "zuerich/plant/1";
 
-// Display 
+// Display
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
@@ -30,24 +29,26 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-//  Setup 
+//  Setup
 void setup() {
   pinMode(GREEN, OUTPUT);
   pinMode(YELLOW, OUTPUT);
   pinMode(RED, OUTPUT);
   pinMode(SENSOR, INPUT);
 
+  Wire.begin(21, 22);
   Serial.begin(115200);
 
   // Display
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    while (1)
+      ;
+  }
   display.clearDisplay();
+  display.display();
 
   // WLAN
-  WiFi.begin(ssid);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-  }
+  setup_wifi();
 
   // MQTT
   client.setServer(mqtt_server, 1883);
@@ -56,14 +57,51 @@ void setup() {
   }
 }
 
-// Loop
+void setup_wifi() {
+  Serial.print("Connecting to ");
+  Serial.print(ssid);
+  WiFi.begin(ssid);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("done!");
+}
+
+void reconnectMQTT() {
+  while (!client.connected()) {
+    Serial.print("MQTT reconnect...");
+    if (client.connect("ESP32_HUMID")) {
+      Serial.println("connected");
+    } else {
+      Serial.println("failed");
+      delay(2000);
+    }
+  }
+}
+
+int readMoisture() {
+  int sum = 0;
+  for (int i = 0; i < 10; i++) {
+    sum += analogRead(SENSOR);
+    delay(5);
+  }
+  return sum / 10;
+}
 void loop() {
+  Serial.println("Loop alive");
+  if (!client.connected()) {
+    reconnectMQTT();
+  }
   client.loop();
 
-  int moisture = analogRead(SENSOR);
+  int moisture = readMoisture();
 
   // Rohdaten
-  client.publish(mqtt_topic, String(moisture).c_str());
+  String payload =  String(moisture);
+
+  client.publish(mqtt_topic, payload.c_str());
+
 
   Serial.print("Current humidity: ");
   Serial.println(moisture);
@@ -77,24 +115,26 @@ void loop() {
   if (moisture < min_humid) {
     // zu trocken
     digitalWrite(GREEN, HIGH);
-  }
-  else if (moisture > max_humid) {
+  } else if (moisture > max_humid) {
     // zu nass
     digitalWrite(RED, HIGH);
-  }
-  else {
+  } else {
     // normal
     digitalWrite(YELLOW, HIGH);
   }
 
   // Display
   display.clearDisplay();
-  display.setTextSize(2);
-  display.setTextColor(WHITE);
+  display.setTextSize(1);
   display.setCursor(0, 0);
-  display.print("Current humidity:");
+  display.print("Moisture:");
+
+  display.setTextSize(2);
+  display.setCursor(0, 16);
   display.print(moisture);
+
   display.display();
+
 
   delay(500);
 }
